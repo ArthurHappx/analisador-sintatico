2d620f902f843eb3cb288f2c373b1e5667076d4d
DanielPDamiao <daniel.pereira.damiao@ccc.ufcg.edu.br>
2025-08-05 23:29:05 -0300

    Versão do parser sintatico sem comentarios e tratamento de erros.

--- src/Parser/Parser.hs
+++ src/Parser/Parser.hs
@@ -1,8 +1,8 @@
 newtype AST = Program [Stmt]
-    deriving (Show)
+    deriving (Show, Eq)
 
 data Stmt = Assign String Expr
-    deriving (Show)
+    deriving (Show, Eq)
 
 data Expr
     = Number String
@@ -12,15 +12,7 @@ data Expr
     | Mult Expr Expr
     | Div Expr Expr
     | UnaryOp String Expr
-    deriving (Show)
-
-{-
-x = 8 + 12
-y = x - 3
-
-TkIdent x, TkAssign, TkNumber 8, TkPlus, TkNumber 12, NEWLINE,
-TkIdent y, TkAssign, TkIdent x, TkMinus, TkNumber 3, EOF
--}
+    deriving (Show, Eq)
 
 data Token
     = TkIdent String
@@ -34,7 +26,7 @@ data Token
     | TkClosePar
     | NEWLINE
     | EOF
-    deriving (Show)
+    deriving (Show, Eq)
 
 
 parseStmt :: [Token] -> Stmt
@@ -43,14 +35,44 @@ parseStmt _ = error "ERROR"
 
 parseExpr :: [Token] -> Expr
 parseExpr x
-    | rest' == [] = term'
-    | head rest' == TkPlus = Plus term' (parseExpr rest')
-    | head rest' == TkMinus = Sub term' (parseExpr rest')
+    | opPos == (-1) = parseTerm x
+    | x !! opPos == TkPlus = Plus (parseExpr(take opPos x)) (parseTerm (drop (opPos+1) x))
+    | x !! opPos == TkMinus = Minus (parseExpr(take opPos x)) (parseTerm (drop (opPos+1) x))
     | otherwise = error "ERROR"
-    where [term', rest'] = parseTerm x --REFATORAÇÃO NECESSÁRIA
+    where opPos =  findExpr (-1) 0 x 
+
+findExpr :: Int -> Int -> [Token] -> Int
+findExpr best pos [h] = best
+findExpr best pos (h:t) 
+    | h == TkOpenPar = findExpr best (pos + parPos) (drop parPos (h:t))
+    | notElem h [TkPlus, TkMinus, TkMult, TkDiv] && head t `elem` [TkPlus, TkMinus] = findExpr (pos+1) (pos+1) t
+    | otherwise = findExpr best (pos+1) t 
+    where parPos = respPar 0 0 (h:t)
 
 parseTerm :: [Token] -> Expr
-parseTerm x = error "NOT IMPLEMENTED YET"
+parseTerm x
+    | opPos == (-1) = parseFactor x
+    | x !! opPos == TkMult = Mult (parseTerm(take opPos x)) (parseFactor (drop (opPos+1) x))
+    | x !! opPos == TkDiv = Div (parseTerm(take opPos x)) (parseFactor (drop (opPos+1) x))
+    | otherwise = error "ERROR"
+    where opPos = findTerm (-1) 0 x 
+
+findTerm :: Int -> Int -> [Token] -> Int
+findTerm best pos [h] = best
+findTerm best pos (h:t) 
+    | h == TkOpenPar = findTerm best (pos + parPos) (drop parPos (h:t))
+    | notElem h [TkPlus, TkMinus, TkMult, TkDiv] && head t `elem` [TkMult, TkDiv] = findTerm (pos+1) (pos+1) t
+    | otherwise = findTerm best (pos+1) t 
+    where parPos = respPar 0 0 (h:t)
+
+respPar :: Int -> Int -> [Token] -> Int
+respPar qnt pos [] = error "ERRO"
+respPar qnt pos (h:t)
+    | (h == TkClosePar) && (qnt-1 == 0) = pos
+    | h == TkClosePar = respPar (qnt-1) (pos+1) t
+    | h == TkOpenPar = respPar (qnt+1) (pos+1) t
+    | otherwise = respPar qnt (pos+1) t
+
 
 parseFactor :: [Token] -> Expr
 parseFactor (TkOpenPar : rest') = parseExpr (safeInit rest')
@@ -63,6 +85,10 @@ safeInit :: [Token] -> [Token]
 safeInit [] = []
 safeInit xs = init xs
 
+safeTail :: [Token] -> [Token] 
+safeTail [] = []
+safeTail xs = tail xs
+
 parseUnary :: [Token] -> Expr
 parseUnary [] = error "ERROR"
 parseUnary (TkMinus : rest') = UnaryOp "-" (parseFactor rest')
@@ -72,14 +98,3 @@ parseValue :: [Token] -> Expr
 parseValue [] = error "ERROR"
 parseValue (TkIdent name : rest') = Var name
 parseValue (TkNumber num : rest') = Number num
-
-
-{-
-area :: Forma -> Float
-area Circle r = 2 * pi * r
-area Retangle a b = a * b
-
-data Forma 
-    = Circle Int
-    | Retangle Int Int 
--}
