4a343f60cc9b67d9ab1165f2518e8b4d136b88f3
DevMoitinho <jotavmb@gmai.com>
2025-08-09 13:01:02 -0300

    Lexer reconhecendo float

--- app/Lexer.hs
+++ app/Lexer.hs
@@ -23,7 +23,7 @@ lexFromFile :: FilePath -> IO (Either String [Token])
 lexFromFile filePath = do
     result <- try (readFile filePath) :: IO (Either IOException String)
     case result of
-        Left err -> return $ Left $ "Erro ao ler arquivo: " ++ show err
+        Left err -> return $ Left $ "ReadFileError " ++ show err
         Right content -> return $ Right $ lexer content
 
 
@@ -44,18 +44,39 @@ lexAll (c:cs)
     | c == '*'               = TkMult : lexAll cs
     | c == '('               = TkOpenPar : lexAll cs
     | c == ')'               = TkClosePar : lexAll cs
-    | otherwise              = error $ "Caractere inesperado '" ++ [c] ++ "'"
+    | otherwise              = error $ "SintaxError: invalid sintax '" ++ [c] ++ "'"
 
 lexIdentifier :: String -> [Token]
 lexIdentifier input =
     let (ident, rest) = span isAlphaNum input
     in TkIdent ident : lexAll rest
 
+-- Implementação atualizada de lexNumber
 lexNumber :: String -> [Token]
-lexNumber input =
-    let (numStr, rest) = span isDigit input
-    in TkNumber numStr : lexAll rest
+lexNumber input = 
+    let (numStr, rest) = span isNumberChar input
+        -- Verifica se é um float válido
+        (isValid, errorMsg) = validateNumber numStr
+    in if isValid
+        then TkNumber numStr : lexAll rest
+        else error errorMsg
 
+-- Validação completa do número
+validateNumber :: String -> (Bool, String)
+validateNumber numStr
+    | hasMultiplePoints = (False, "SyntaxError: invalid syntax'")
+    | hasLeadingZeros   = (False, "SyntaxError: leading zeros in decimal integer literals are not permitted'" ++ numStr ++ "'")
+    | otherwise         = (True, "")
+    where
+        hasMultiplePoints = length (filter (== '.') numStr) > 1
+        hasLeadingZeros = case numStr of
+            '0':'.':_ -> False  -- 0.x é válido
+            '0':x:_    -> x /= '.' && isDigit x 
+            _          -> False
+
+-- Função auxiliar para caracteres válidos em números
+isNumberChar :: Char -> Bool
+isNumberChar c = isDigit c || c == '.'
 
 
 isAlphaNum :: Char -> Bool
