68a4755027ff7a1a659e95a6d72d648d8558da81
Arthyp <114695733+Arthyp@users.noreply.github.com>
2025-08-09 20:51:12 -0300

    Update Main.hs

--- app/Main.hs
+++ app/Main.hs
@@ -1,46 +1,9 @@
 module Main where
 
-import CLI.Args (parseArgs, Command(..))
-import CLI.Commands (mostrarAjuda, executarTestes)
-import Parser.Parser (programParser)
-import AST.VisualAST (showAST, saveAST)
-import Lexer.Lexer (lexer) 
-
-import System.Directory (doesFileExist)
+import CLI.Args (parseArgs)
+import CLI.Commands (executarComando)
 
 main :: IO ()
 main = do
     cmd <- parseArgs
-    case cmd of
-        Analyze file -> analisarArquivo file
-        AnalyzeSimple file -> analisarArquivoSimples file
-        Help -> mostrarAjuda
-        Tests args -> executarTestes args
-        Invalid -> putStrLn "Comando inválido. Use --help para ver as opções."
-
--- Análise completa + salva AST em arquivo
-analisarArquivo :: FilePath -> IO ()
-analisarArquivo file = do
-    existe <- doesFileExist file
-    if existe
-        then do
-            code <- readFile file
-            let tokens = lexer code
-            let ast = programParser tokens
-            putStrLn "AST gerada:"
-            putStrLn (showAST ast)
-            saveAST ast "resultado_ast.txt"
-            putStrLn "AST salva em resultado_ast.txt"
-        else putStrLn "Arquivo não encontrado."
-
--- Análise simplificada (só mostra AST no terminal)
-analisarArquivoSimples :: FilePath -> IO ()
-analisarArquivoSimples file = do
-    existe <- doesFileExist file
-    if existe
-        then do
-            code <- readFile file
-            let tokens = lexer code
-            let ast = programParser tokens
-            putStrLn (showAST ast)
-        else putStrLn "Arquivo não encontrado."
+    executarComando cmd
