5537abf444f3afbb02b5f76b14222525610feea1
ArthurHappx <arthurfelixdesiqueira@gmail.com>
2025-08-11 16:46:11 -0300

    TestRunner e tests modificados

--- test/TestRunner.hs
+++ test/TestRunner.hs
@@ -1,23 +1,21 @@
---testes manuais, para roda-los corretamente faça 'ghci -isrc test/TestRunner.hs'
---assim 'runAll' dave atualizar os resultados em './Logs'
-
 module TestRunner ( main, runAll ) where
 
 import Control.Exception
 import Parser.Parser ( programParser )
 import AST.VisualAST ( saveAST )
 import Lexer.Lexer ( lexFromFile )
+import System.FilePath (takeBaseName)
 
-main :: Int -> IO()
-main num = do
-    lexerResult <- lexFromFile ("./test/ex" ++ show num ++ ".py")
+main :: FilePath -> IO()
+main path = do
+    lexerResult <- lexFromFile ("./test/Examples/" ++ path)
     case lexerResult of
         Left err -> putStrLn err
         Right tokens -> (do
             ast <- evaluate(programParser tokens) 
-            saveAST ast ("./test/Logs/ex" ++ show num ++ "_ast.txt")) `catch` \e -> writeFile ("./test/Logs/ex" ++ show num ++ "_ast.txt") ("Erro ao analisar o arquivo: " ++ show (e :: ErrorCall))
+            saveAST ast ("./test/Logs/" ++ takeBaseName path ++ "_ast.txt")) `catch` \e -> writeFile ("./test/Logs/" ++ takeBaseName path ++ "_ast.txt") ("Erro ao analisar o arquivo: " ++ show (e :: ErrorCall))
 
-runAll :: [Int] -> IO()
+runAll :: [FilePath] -> IO()
 runAll [] = return ()
 runAll (h:t) = do
     main h
