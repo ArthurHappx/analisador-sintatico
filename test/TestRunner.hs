34a9c164e92007f0b9ca5f82d38b397e4a3890bc
ArthurHappx <arthurfelixdesiqueira@gmail.com>
2025-09-15 16:41:30 -0300

    TestRunner modificado para salvar tokens nos logs

--- test/TestRunner.hs
+++ test/TestRunner.hs
@@ -12,8 +12,10 @@ main path = do
     case lexerResult of
         Left err -> putStrLn err
         Right tokens -> (do
+            writeFile ("./test/Logs/" ++ takeBaseName path ++ "_tokens.txt") (show tokens ++ "\n")
             ast <- evaluate(programParser tokens) 
             saveAST ast ("./test/Logs/" ++ takeBaseName path ++ "_ast.txt")) `catch` \e -> writeFile ("./test/Logs/" ++ takeBaseName path ++ "_ast.txt") ("Erro ao analisar o arquivo: " ++ show (e :: ErrorCall))
+           
 
 runAll :: [FilePath] -> IO()
 runAll [] = return ()
